899ec2a802a493f51e80a7288028233aabfa1cfb
froliceric0914 <wzhao.eric@gmail.com>
2019-06-25 19:09:24 -0400

    remove the class for studentList

--- src/component/StudentsList.jsx
+++ src/component/StudentsList.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import Student from './Student.jsx';
 
-class StudentsList extends Component {
-    render() {
-        const studentsList = this.props.students.map((student, index) => {
-            return (
-                <Student
-                    toggleHidden={this.props.toggleHidden}
-                    newTag={this.props.newTag}
-                    student={student}
-                    key={index}
-                    index={index}
-                />
-            );
-        });
+// class StudentsList extends Component {
+//     render() {
 
-        //need to add a contion when there is no sudent after filter
-        return <div className="student-list">{studentsList}</div>;
-    }
-}
-
-export default StudentsList;
+export default ({ students, toggleHidden, newTag, ...props }) => {
+    const studentsList = students.map((student, index) => {
+        return (
+            <Student
+                toggleHidden={toggleHidden}
+                newTag={newTag}
+                student={student}
+                key={index}
+                index={index}
+            />
+        );
+    });
+    //need to add a contion when there is no sudent after filter
+    return <div className="student-list">{studentsList}</div>;
+};
